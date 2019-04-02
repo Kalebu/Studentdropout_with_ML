2fad12f9ff273198ebfb74c6a35988604d53054a
kalebu Gwalugano <kalebjordan.kj@gmail.com>
2019-04-03 00:22:13 +0300

    Database

--- scripts/tabledef.py
+++ scripts/tabledef.py
@@ -1,34 +1,29 @@
-# -*- coding: utf-8 -*-
-
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///accounts.db'
+#=======================Database initialization======================
+SQLALCHEMY_DATABASE_URI = 'sqlite:///student.db'
 
 Base = declarative_base()
 
-
+#======================function to create a connection to a Database===============
 def db_connect():
-    """
-    Performs database connection using database settings from settings.py.
-    Returns sqlalchemy engine instance
-    """
     return create_engine(SQLALCHEMY_DATABASE_URI)
 
 
+#=====================Function to create a Database using ORM====================
 class User(Base):
     __tablename__ = "user"
 
     id = Column(Integer, primary_key=True)
-    username = Column(String(30), unique=True)
-    password = Column(String(30))
-    email = Column(String(50))
+    username = Column("Admin_name", String(30), unique=True)
+    password = Column("Admin_password", String(30), nullable=False)
+    email = Column("Admin_email", String(50), nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
-
 
 engine = db_connect()  # Connect to database
 Base.metadata.create_all(engine)  # Create models
